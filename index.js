575dad1d7ad2323afd27d5b3a4101d9e64e85a46
xzzaaa <133853510+xzzaaa@users.noreply.github.com>
2024-11-25 15:10:24 +0100

    thing that works

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use('/public', express.static('public'))
 app.get('/', function(req,res) {
     res.render('index');
 });
+app.get('/login', function(req,res) {
+    res.render('login');
+});
+app.get('/signup', function(req,res) {
+    res.render('register');
+});
 
 app.listen(port, function(){
     console.log(`Servern körs på http://localhost:${port}`);
